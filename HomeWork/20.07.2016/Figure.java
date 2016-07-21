f2e9cef8ea972a2291fae0b87a28ea18a9d25ff6
Silur <proSilur@gmail.com>
2016-07-21 13:46:34 +0300

    Update Figure.java

--- HomeWork/20.07.2016/Figure.java
+++ HomeWork/20.07.2016/Figure.java
@@ -16,19 +16,37 @@ import java.util.Scanner;
 public class Figure {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder builder = new StringBuilder();
         System.out.println("Enter height of figure: ");
         int length = scanner.nextInt();
+        int counter = 0;
+        int counter2 = 0;
 
         //Вывод в консоль одним циклом
-        for (int i = 0; i < length * 2 - 1; i++) {
-            if (i < length) {
-                System.out.println(builder.append("*"));
+        //Чуть более чем полностью вывихнул моск, но вроди работает
+
+        for (int i = 0; i < length * length; ) {
+            counter2++;
+            System.out.print("*");
+            if (counter2 > length * length - 1) break;
+            if (counter2 >= length + (length * length - length) / 2) {
+                if (counter + 1 == length) {
+                    System.out.println();
+                    counter = i - counter + 1;
+                    i++;
+                } else {
+                    counter++;
+                }
             } else {
-                System.out.println(builder.deleteCharAt(0));
+                if (i == counter) {
+                    System.out.println();
+                    counter = 0;
+                    i++;
+                } else {
+                    counter++;
+                }
             }
         }
         scanner.close();
     }
-}
 
+}
