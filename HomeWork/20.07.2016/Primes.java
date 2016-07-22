0d206716dc163028fbde82f465665527c83ec6c3
Silur <proSilur@gmail.com>
2016-07-22 13:17:32 +0300

    Update Primes.java

--- HomeWork/20.07.2016/Primes.java
+++ HomeWork/20.07.2016/Primes.java
@@ -4,33 +4,15 @@
     само на себя. Первые простые числа это — 2,3,5,7...
  */
 
-
 public class Primes {
     public static void main(String[] args) {
-
-        // Решето Эратосфена
-        boolean[] array = new boolean[100];
-        // array[0] = array[1] = false;
-        for (int i = 2; i < array.length; i++) {
-            array[i] = true;
-        }
-        for (int i = 2; i < array.length; i++) {
-            if (array[i]) {
-                for (int j = 2; j * i < array.length; j++) {
-                    array[i * j] = false;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (j != i && i % j == 0) {
+                    break;
+                } else if (j == i) {
+                    System.out.print(i + " ");
                 }
-            }
-        }
-
-        // Print our numbers in three columns for clarity :)
-        int temp = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] && temp < 3) {
-                System.out.print(i + "\t");
-                temp++;
-            } else if (i % 10 == 0 && temp == 3) {
-                System.out.println();
-                temp = 0;
             }
         }
     }
