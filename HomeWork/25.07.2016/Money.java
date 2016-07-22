9f52bfa95319b072d2091d3d090f8e4f25148704
Silur <proSilur@gmail.com>
2016-07-22 18:41:57 +0300

    Update Money.java

--- HomeWork/25.07.2016/Money.java
+++ HomeWork/25.07.2016/Money.java
@@ -8,13 +8,24 @@
  You have: one hundred twenty three dollars thirty four cents
  */
 
+import java.util.Scanner;
+
 public class Money {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String[] ten = {"one, two, three, four, five, six, seven, eight, nine"};
         String[] twenty = {"eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen"};
         String[] hundred = {"ten, twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety"};
         String[] million = {"hundred, thousand, million"};
         String[] money = {"dollar", "dollars", "cent", "cents"};
+        String number = scanner.nextLine();
+
+        for (int i = 0; i < number.length(); i++) {
+            if(number.contains(".") || number.contains(",")){
+
+            }
+        }
+
 
     }
 }
