de6a487f41e24ebf5cb4ce1be5c2dc7d4c95b1c7
proSilur <prosilur@gmail.com>
2016-07-22 16:32:25 +0300

    комит

---
+++ HomeWork/25.07.2016/Money.java
@@ -0,0 +1,17 @@
+package third;
+
+/*
+ 3)Ввести с клавиатуры число (до миллиарда) которое обозначает
+ количество долларов и центов пользователя. Вывести это количество
+ прописью. (4 часа)
+ Например:
+ How much money do you have?
+ 123,34
+ You have: one hundred twenty three dollars thirty four cents
+ */
+
+public class Money {
+    public static void main(String[] args) {
+
+    }
+}
