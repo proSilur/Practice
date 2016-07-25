6f4baa3f6112174dbda2886b7b5bc57da5a3465c
Silur <proSilur@gmail.com>
2016-07-26 01:07:45 +0400

    Update OddNumbers.java

--- HomeWork/25.07.2016/OddNumbers.java
+++ HomeWork/25.07.2016/OddNumbers.java
@@ -7,7 +7,6 @@ public class OddNumbers {
     public static void main(String[] args) {
         int array[] = {0, 5, 2, 4, 7, 1, 3, 19};
         int counter = 0;
-        //Ноль не учитываю
         for (int anArray : array) {
             if (anArray % 2 != 0) {
                 counter++;
