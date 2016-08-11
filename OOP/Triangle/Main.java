8361bb2e9ef59b213532583214abe6e44c115cd6
Silur <proSilur@gmail.com>
2016-08-11 18:26:35 +0300

    Update Main.java

--- OOP/Triangle/Main.java
+++ OOP/Triangle/Main.java
@@ -1,8 +1,4 @@
-package OppOne;
 
-/**
- * Created by George on 11.08.2016.
- */
 public class Main {
     public static void main(String[] args) {
         Triangle triangleOne = new Triangle(3,4,0);
